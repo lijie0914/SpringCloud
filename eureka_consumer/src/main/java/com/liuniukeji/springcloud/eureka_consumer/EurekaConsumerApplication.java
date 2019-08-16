a6a5b2c875dbfe8b763ab598c5ee6c37aec33691
lijie <13589214035@163.com>
2019-08-16 10:36:05 +0800

    实现对Hystrix的实时监控可视化界面

--- eureka_consumer/src/main/java/com/liuniukeji/springcloud/eureka_consumer/EurekaConsumerApplication.java
+++ eureka_consumer/src/main/java/com/liuniukeji/springcloud/eureka_consumer/EurekaConsumerApplication.java
@@ -3,22 +3,28 @@ package com.liuniukeji.springcloud.eureka_consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableFeignClients
 @EnableZuulProxy
 @EnableHystrix
 @SpringBootApplication
 public class EurekaConsumerApplication {
 
-    // @EnableFeignClients 开启feign
-    // @EnableZuulProxy 声明开启zuul
-    // @EnableHystrix 开启Hystrix熔断器功能
+    // @EnableCircuitBreaker 启动断路器 （和@EnableHystrix功能一样，两者选一即可）
+    // @EnableHystrixDashboard 开启对Hystrix的实时监控
+    // @EnableFeignClients 开启feign 服务调用
+    // @EnableZuulProxy 声明开启zuul API网关
+    // @EnableHystrix 开启Hystrix熔断器功能 （和@EnableCircuitBreaker功能一样，两者选一即可）
     // PS：从Spring Cloud Edgware版本开始，eureka的服务提供者/服务消费者的启动类上不需要添加@EnableDiscoveryClient 或@EnableEurekaClient
     // 当前应用为服务消费者，负责消费（即调用）eureka中的服务生产者，
     // 同时还整合ribbon实现了本地的负载均衡，
     // 整合hystrix实现了服务调用的容错保护机制（基于restTemplate方式实现的服务降级功能），
+    // 整合hystrix-dashboard实现了对hystrix的实时监控可视化界面（访问地址：http://localhost:9001/hystrix）
     // 整合feign实现了服务调用（ribbon也支持通过restTemplate实现服务调用）以及基于hystrix的服务降级功能，
     // 整合zuul实现了API网关功能。
 
